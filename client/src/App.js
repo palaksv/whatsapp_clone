d097294e08f031b5f14542a484549e50d4794734
palak <palakhandelwalgp@gmail.com>
2023-02-03 13:21:49 +0530

    added login frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 
 import Messenger from "./components/Messenger";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 function App() {
+
+  const clientId="930280719874-80e5d142rd3rdg7jhhvrkepuirrttfeu.apps.googleusercontent.com"
   return (
-    <div >
+    <GoogleOAuthProvider clientId={clientId} >
       <Messenger></Messenger>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
--- client/src/components/Messenger.jsx
+++ client/src/components/Messenger.jsx
@@ -1,12 +1,29 @@
+import LoginDialog from "./account/LoginDialog";
+import { AppBar, Toolbar , styled ,Box} from "@mui/material";
 
 
 
-const Messenger=()=>{
-    return(
-        <>Hello
-        </>
-    )
-}
 
+const Component=styled(Box)`
+height:100vh;
+background-color:#DCDCDC;
+`
+
+const Header=styled(AppBar)`
+height : 200px;
+background-color : #00bfa5;
+box-shadow:none;
+`
+
+const Messenger = () => {
+  return (
+    <Component>
+      <Header>
+        <Toolbar></Toolbar>
+      </Header>
+      <LoginDialog />
+    </Component>
+  );
+};
 
 export default Messenger;
