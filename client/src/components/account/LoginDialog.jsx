b915c631610c783d37227485d46a1c790cdd1ac8
palak <palakhandelwalgp@gmail.com>
2023-02-07 23:47:53 +0530

    added server

--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -4,6 +4,8 @@ import {GoogleLogin} from '@react-oauth/google';
 import jwt_decode from 'jwt-decode';
 import { useContext } from "react";
 import { AccountContext } from "../../context/AccountProvider";
+import { addUser } from "../../service/api";
+
 
 const Component=styled(Box)`
 display:flex
@@ -53,10 +55,11 @@ const LoginDialog=()=>{
 const {setAccount} = useContext(AccountContext);
 
 
- const onLoginSuccess=(res)=>{
+ const onLoginSuccess=async (res)=>{
     const decoded=jwt_decode(res.credential);
     console.log(decoded);
     setAccount(decoded);
+    await addUser(decoded);
  }
 
  const onLoginError=(res)=>{
